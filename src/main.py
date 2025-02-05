b33cf31f479ff817fd6e7878c0a42c1eec14155a
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-04 23:28:23 -0500

    Cleaning some data

--- src/main.py
+++ src/main.py
@@ -5,16 +5,12 @@ from src.models.logic.force import BruteForce
 
 def start_up():
     """Punto de entrada principal"""
-    # ABCD #
+    #                ABCD #
     estado_inicio = "1000"
-    condiciones = "1110"
-    alcance = "1110"
-    mechanismo = "1110"
 
     sys_config = Manager(estado_inicial=estado_inicio)
 
     ### Ejemplo de solución mediante módulo de pyphi ###
 
     analizador_fb = BruteForce(sys_config)
-    sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mechanismo)
-    print(sia_uno)
+    analizador_fb.analizar_completamente_una_red()
