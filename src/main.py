53555227b106fbd07557b40490ce403189b46040
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-08 15:33:34 -0500

    Fore white last key

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
-from src.controllers.manager  import Manager
+from src.controllers.manager import Manager
 
 from src.models.strategies.force import BruteForce
 
 
 def start_up():
     """Punto de entrada principal"""
-                   # ABCD #
+    # ABCD #
     estado_inicio = "1000"
+    condiciones = "1110"
+    alcance = "1110"
+    mechanismo = "1110"
+
     config_sistema = Manager(estado_inicial=estado_inicio)
 
-    ## Ejemplo de solución mediante fuerza bruta ##
+    ### Ejemplo de solución mediante módulo de fuerza bruta ###
 
     analizador_fb = BruteForce(config_sistema)
-    analizador_fb.analizar_completamente_una_red()
+    sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mechanismo)
+    print(sia_uno)
