b4af50ac2358769cc214e6c316f428120c9ab6ed
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-07 21:31:30 -0500

    main restored

--- src/main.py
+++ src/main.py
@@ -1,21 +1,15 @@
-from models.strategies.force import BruteForce
 from src.controllers.manager import Manager
 
-from src.models.strategies.phi import Phi
+from src.models.strategies.force import BruteForce
 
 
 def start_up():
     """Punto de entrada principal"""
-    # ABCD #
+    #                ABCD #
     estado_inicio = "1000"
-    condiciones = "1110"
-    mechanismo = "0110"
-    alcance = "1010"
-
     config_sistema = Manager(estado_inicial=estado_inicio)
 
-    ### Ejemplo de solución mediante Pyphi ###
+    ## Ejemplo de solución mediante fuerza bruta ##
 
     analizador_fb = BruteForce(config_sistema)
-    sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mechanismo)
-    print(sia_uno)
+    analizador_fb.analizar_completamente_una_red()
