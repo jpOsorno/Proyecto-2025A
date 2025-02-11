38a6f617cb134a31d45375a67e0546cd79da94e9
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-11 15:43:55 -0500

    Now isn't never gona crash, forever in life

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from src.controllers.strategies.phi import Phi
+from src.controllers.strategies.q_nodes import QNodes
 from src.controllers.manager import Manager
+from src.controllers.strategies.phi import Phi
 from src.controllers.strategies.force import BruteForce
 
 
@@ -14,6 +15,6 @@ def iniciar():
     config_sistema = Manager(estado_inicial=estado_inicio)
 
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = Phi(config_sistema)
+    analizador_fb = QNodes(config_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(condiciones, alcance, mecanismo)
     print(sia_uno)
