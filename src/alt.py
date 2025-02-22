62ae1fd4972aef3772f4fc4e15e6981f45e8b7d9
OH <over.castrillon22396@ucaldas.edu.co>
2025-02-22 17:08:53 -0500

    save

--- src/alt.py
+++ src/alt.py
@@ -1,5 +1,6 @@
 from src.controllers.strategies.q_nodes import QNodes
 from src.middlewares.slogger import SafeLogger
+from src.controllers.manager import Manager
 
 from src.testing.data import (
     NUM_NODOS,
@@ -12,10 +13,6 @@ from src.constants.models import (
     QNODES_STRAREGY_TAG,
 )
 
-from src.controllers.manager import Manager
-# from src.controllers.strategies.phi import Phi
-
-# En tu función principal:
 # from src.testing.result import GestorResultados  # Ajusta la ruta según tu estructura
 
 
@@ -62,47 +59,3 @@ def iniciar():
                 solucion.perdida,
                 solucion.tiempo_ejecucion,
             )
-            break
-        break
-
-
-# def iniciar():
-#     """Punto de entrada principal"""
-#     red_usada = RED_05
-
-#     muestras: list[list[tuple[str, str]]] = red_usada[PRUEBAS]
-#     num_nodos: int = red_usada[NUM_NODOS]
-
-#     estado_inicio = f"1{'0' * (num_nodos - 1)}"
-#     condiciones = "1" * num_nodos
-
-#     config_sistema = Manager(estado_inicial=estado_inicio)
-#     # Cargar resultados previos
-#     soluciones = cargar_resultados_existentes()
-
-#     logger = SafeLogger(QNODES_STRAREGY_TAG)
-#     for lote in muestras:
-#         for prueba in lote:
-#             if prueba in soluciones.keys():
-#                 # Si ya está guardado, saltamos
-#                 continue
-#             logger.error(f"\n{prueba=}")
-
-#             alcance, mecanismo = prueba
-#             analizador_q = Phi(config_sistema)
-
-#             solucion = analizador_q.aplicar_estrategia(
-#                 condiciones,
-#                 alcance,
-#                 mecanismo,
-#             )
-
-#             # Guardar en memoria
-#             soluciones[prueba] = (
-#                 solucion.perdida,
-#                 solucion.tiempo_ejecucion,
-#             )
-#             # Guardar inmediatamente para minimizar pérdida en caso de crash
-#             guardar_resultados(soluciones)
-#             # break  # Eliminar cuando esté listo
-#         break  # Eliminar cuando esté listo
