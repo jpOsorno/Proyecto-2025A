8ee3dd225bad5effad288580f6ad488b89479682
Juan pablo Osorno <jpog0411@gmail.com>
2025-04-10 01:56:48 -0500

    Mas optimizado que 10

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
 from src.controllers.manager import Manager
 
 from src.controllers.strategies.force import BruteForce
+from src.controllers.strategies.q_nodes import QNodes
 
 
 def iniciar():
     """Punto de entrada principal"""
-                    # ABCD #
-    estado_inicial = "1000"
-    condiciones =    "1110"
-    alcance =        "1110"
-    mecanismo =      "1110"
+                    # ABCDEFGHIJ #
+    # estado_inicial = "1000000000"
+    # condiciones =    "1111111111"
+    # alcance =        "1111111111"
+    # mecanismo =      "1111111111"
+
+    estado_inicial = "100000000000000"
+    condiciones =    "111111111111111"
+    alcance =        "111111111111111"
+    mecanismo =      "111111111111111"
+
+    # estado_inicial = "10000000000000000000"
+    # condiciones =    "11111111111111111111"
+    # alcance =        "11111111111111111111"
+    # mecanismo =      "11111111111111111110"
 
     gestor_sistema = Manager(estado_inicial)
 
     ### Ejemplo de solución mediante módulo de fuerza bruta ###
-    analizador_fb = BruteForce(gestor_sistema)
+    analizador_fb = QNodes(gestor_sistema)
     sia_uno = analizador_fb.aplicar_estrategia(
         condiciones,
         alcance,
